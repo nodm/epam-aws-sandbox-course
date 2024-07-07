08ae1548ccd72b35b5fa19e11495ddb5e2b7cf9e
Dmytro Novikov <nodm.pe@gmail.com>
2024-07-07 14:29:30 +0300

    task01: Remove redundant quotation marks

--- task01/app/lambdas/hello_world/index.js
+++ task01/app/lambdas/hello_world/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    message: JSON.stringify('Hello from Lambda!'),
+    message: 'Hello from Lambda!',
   };
 
   return response;
